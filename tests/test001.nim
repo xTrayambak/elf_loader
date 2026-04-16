a9c9aa4f35ef839857e181d0be77573aa0c2c937
xTrayambak <xtrayambak@gmail.com>
2026-04-16 21:53:09 +0530

    add: tests: more stuff

--- tests/test001.nim
+++ tests/test001.nim
@@ -1,10 +1,18 @@
-import pkg/elf_loader
+import std/[strformat, posix]
+import pkg/elf_loader, pkg/elf_loader/gnu_hash
 import pkg/[pretty, results]
-import common, posix, strformat
+import common
 
-let lib = loadLibraryAbs(getLibcPath())
+let lib = loadLibraryAbs(
+  getLibcPath(),
+  LoaderCallbacks(
+    resolveSymbol: proc(name: string): pointer =
+      echo &"Resolve symbol '{name}'"
+      # a very stupid shim to just use the pre-existing glibc versions of these symbols
+      dlsym(nil, cstring(name))
+  ),
+)
 if isErr(lib):
-  echo "failed"
   echo lib.error()
 else:
   echo "done"
@@ -14,6 +22,9 @@ let libc = get lib
 let sym = cast[proc(path: cstring, flags: int32, mode: uint16): int32 {.cdecl.}](symAddr(
   libc, "open"
 ))
+let sym2 = cast[proc(fd: int32, buf: cstring, cnt: uint64): int64 {.cdecl.}](symAddr(
+  libc, "write"
+))
 assert sym != nil
-let x = sym("/dev/stdou".cstring, O_RDONLY, 0)
-echo x
+let x = sym("/dev/stdout".cstring, O_RDWR, 0)
+discard sym2(x, cstring("hello elf_loader! :P\n"), 21)
